e2a42fc51e45f95f1e5127b115be20d76a50c19d
Rick Moran <drhayt@gmail.com>
2017-05-02 19:10:10 -0400

    Moved things outside.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
-	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"path"
 
-	respond "gopkg.in/matryer/respond.v1"
-
+	"github.com/drhayt/coatlocker/pkg/fshandler"
 	hndl "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
-
-type Server struct {
-	BaseDirectory string
-}
 
 func main() {
 
@@ -32,7 +22,7 @@ func main() {
 	flag.Parse()
 
 	// Get a copy of the server struct to work with
-	server := Server{BaseDirectory: *baseDirectory}
+	server := fshandler.Server{BaseDirectory: *baseDirectory}
 
 	// Validate our server config.
 	err := server.Validate()
@@ -46,9 +36,9 @@ func main() {
 
 	//Users
 	router.HandleFunc("/health", server.HealthEndpoint).Methods("GET")
-	router.PathPrefix("/").HandlerFunc(server.GetFileEndpoint).Methods("GET")
-	router.PathPrefix("/").HandlerFunc(server.PutFileEndpoint).Methods("PUT")
-	router.PathPrefix("/").HandlerFunc(server.DeleteFileEndpoint).Methods("DELETE")
+	router.PathPrefix("/").HandlerFunc(server.GetEndpoint).Methods("GET")
+	router.PathPrefix("/").HandlerFunc(server.PutEndpoint).Methods("PUT")
+	router.PathPrefix("/").HandlerFunc(server.DeleteEndpoint).Methods("DELETE")
 
 	// This sets up the application to serve.
 	go http.ListenAndServe(":8887", nil) // For net/trace
@@ -59,156 +49,4 @@ func main() {
 
 	// We should setup an additional GRPC endpoint.
 
-}
-
-// HealthEndpoint is an endpoint to allow for health monitoring.
-func (s *Server) HealthEndpoint(w http.ResponseWriter, r *http.Request) {
-	respond.WithStatus(w, r, http.StatusOK)
-
-}
-
-// DeleteFileEndpoint handles deleting a file if it exists.
-func (s *Server) DeleteFileEndpoint(w http.ResponseWriter, r *http.Request) {
-
-	key := s.GenKey(r.RequestURI)
-	filepath := s.GenPath(key)
-
-	// Dont try to get a file that does not exists.
-	err := checkFile(filepath)
-	if err != nil {
-		respond.WithStatus(w, r, http.StatusNotFound)
-		return
-	}
-
-	// Ok, its there, actually remove it.
-	err = os.Remove(filepath)
-	if err != nil {
-		respond.WithStatus(w, r, http.StatusInternalServerError)
-		return
-	}
-	respond.WithStatus(w, r, http.StatusOK)
-}
-
-func (s *Server) GetFileEndpoint(w http.ResponseWriter, r *http.Request) {
-
-	key := s.GenKey(r.RequestURI)
-	filepath := s.GenPath(key)
-
-	// Dont try to get a file that does not exists.
-	err := checkFile(filepath)
-	if err != nil {
-		respond.WithStatus(w, r, http.StatusNotFound)
-		return
-	}
-
-	// It must exist, so open it up.
-	file, err := os.Open(filepath)
-	if err != nil {
-		respond.WithStatus(w, r, http.StatusInternalServerError)
-		return
-	}
-	defer file.Close()
-
-	// Stuff must be good.
-	w.WriteHeader(http.StatusOK)
-	// Lets stream some bytes.
-	// _, err = io.Copy(w, file)
-	// we dont care if this fails.
-	// respond.With(w, r, http.StatusInternalServerError, file.
-	io.Copy(w, file)
-	return
-}
-
-func (s *Server) PutFileEndpoint(w http.ResponseWriter, r *http.Request) {
-
-	key := s.GenKey(r.RequestURI)
-	filepath := s.GenPath(key)
-
-	// Dont try to put a file that already exists.
-	err := checkFile(filepath)
-	if err == nil {
-		respond.WithStatus(w, r, http.StatusUnprocessableEntity)
-		return
-	}
-
-	file, err := os.Create(filepath)
-	if err != nil {
-		respond.WithStatus(w, r, http.StatusInternalServerError)
-		return
-	}
-	defer file.Close()
-	_, err = io.Copy(file, r.Body)
-	if err != nil {
-		respond.WithStatus(w, r, http.StatusInternalServerError)
-		return
-	}
-	respond.WithStatus(w, r, http.StatusCreated)
-
-}
-
-func (s *Server) Validate() error {
-	err := checkDir(s.BaseDirectory)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s Server) GenKey(URI string) string {
-	hasher := sha256.New()
-	hasher.Write([]byte(URI))
-	key := hex.EncodeToString(hasher.Sum(nil))
-	return (key)
-}
-
-func (s Server) GenPath(Key string) string {
-	return path.Join(s.BaseDirectory, Key)
-}
-
-// Err unless a directory exists.
-func verifyDirectoryExists(path string) error {
-	DirStat, err := os.Stat(path)
-	if err != nil {
-		return err
-	}
-	if !DirStat.IsDir() {
-		return errors.New(fmt.Sprintf("%s: %s", path, "is not a directory"))
-	}
-	return nil
-}
-
-// Err unless a directory exists.
-func checkDir(path string) error {
-	DirStat, err := os.Stat(path)
-	if err != nil {
-		return err
-	}
-	if !DirStat.IsDir() {
-		return errors.New(fmt.Sprintf("%s: %s", path, "is not a directory"))
-	}
-	return nil
-}
-
-// Err unless a file exists.
-func verifyFileExists(path string) error {
-	FileStat, err := os.Stat(path)
-	if err != nil {
-		return err
-	}
-	if FileStat.IsDir() {
-		return errors.New(fmt.Sprintf("%s: %s", path, "is not a file"))
-	}
-	return nil
-}
-
-// Err unless a file exists.
-func checkFile(path string) error {
-	FileStat, err := os.Stat(path)
-	if err != nil {
-		return err
-	}
-	if FileStat.IsDir() {
-		return errors.New(fmt.Sprintf("%s: %s", path, "is not a file"))
-	}
-	return nil
 }
