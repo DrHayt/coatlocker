c5c87a06855112275185649113143a4388d1e0d3
Rick Moran <drhayt@gmail.com>
2017-05-02 20:30:33 -0400

    Retrieve the certificate.

--- pkg/jwtclient/jwtclient.go
+++ pkg/jwtclient/jwtclient.go
@@ -45,3 +45,39 @@ func Authenticate(insecure bool, url, username, password string) (token string, 
 	token = buffer.String()
 	return
 }
+
+// RetrieveCertificate is a wrapper to retrieve a certificate from a remote server.
+func RetrieveCertificate(insecure bool, url string) (certificate string, err error) {
+
+	// Setup the request
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+
+	// Get a config to override potential SSL stuff.
+	tlsConfig := &tls.Config{}
+
+	// If we are insecure, then so be it.
+	if insecure {
+		tlsConfig.InsecureSkipVerify = true
+	}
+
+	// Create the transport
+	tr := &http.Transport{TLSClientConfig: tlsConfig}
+
+	// Create a client using that transport.
+	client := &http.Client{Transport: tr}
+
+	// Make the request
+	webResponse, err := client.Do(request)
+	if err != nil {
+		return
+	}
+
+	defer webResponse.Body.Close()
+	buffer := &bytes.Buffer{}
+	io.Copy(buffer, webResponse.Body)
+	certificate = buffer.String()
+	return
+}
